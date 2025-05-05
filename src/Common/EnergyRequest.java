e03ccc7908073abdc436bb66d8ec3e2e0ee34c69
Yagiz Tansu <yagiz.tansu99@gmail.com>
2025-05-05 02:24:03 +0200

    Add energy request publishing functionality

--- src/Common/EnergyRequest.java
+++ src/Common/EnergyRequest.java
@@ -1,4 +1,22 @@
 package Common;
 
 public class EnergyRequest {
+    public int amount;
+    public long timestamp;
+
+    public EnergyRequest(int amount, long timestamp) {
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    //serialization function
+    public String toString() {
+        return amount + " " + timestamp;
+    }
+
+    //deserialization
+    public static EnergyRequest fromString(String s) {
+        String[] parts = s.split(" ");
+        return new EnergyRequest(Integer.parseInt(parts[0]), Long.parseLong(parts[1]));
+    }
 }
--- src/RenewableEnergyProvider/RenewableEnergyProvider.java
+++ src/RenewableEnergyProvider/RenewableEnergyProvider.java
@@ -1,4 +1,27 @@
 package RenewableEnergyProvider;
 
+import Common.EnergyRequest;
+import Mqtt.MqttPublisher;
+
 public class RenewableEnergyProvider {
+    private final MqttPublisher mqttPublisher;
+
+    public RenewableEnergyProvider(MqttPublisher mqttPublisher) {
+        this.mqttPublisher = mqttPublisher;
+    }
+
+    private int random(int min, int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
+
+    public void publishRequest() {
+        int kWh = random(5000, 15000);
+        EnergyRequest req = new EnergyRequest(kWh, System.currentTimeMillis());
+
+        mqttPublisher.publish("RenewableEnergyProvider/request", req.toString());
+    }
+
+
+
+
 }
