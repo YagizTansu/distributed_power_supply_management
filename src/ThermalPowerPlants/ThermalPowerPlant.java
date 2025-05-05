9dcc63aa596cb4b50c26f0026075394abd9541fa
Yagiz Tansu <yagiz.tansu99@gmail.com>
2025-05-05 02:47:52 +0200

    Add MQTT integration to ThermalPowerPlant class

--- src/ThermalPowerPlants/ThermalPowerPlant.java
+++ src/ThermalPowerPlants/ThermalPowerPlant.java
@@ -1,4 +1,39 @@
 package ThermalPowerPlants;
 
+import Mqtt.MqttSubscriber;
+
 public class ThermalPowerPlant {
+    private String name;
+    private String id;
+    private final MqttSubscriber mqttSubscriber;
+
+    public ThermalPowerPlant(String name, String id, MqttSubscriber mqttSubscriber) {
+        this.name = name;
+        this.id = id;
+        this.mqttSubscriber = mqttSubscriber;
+    }
+
+    public void subscribeRenewableEnergyProviderRequest() {
+        mqttSubscriber.subscribe("renewableEnergyProvider/request");
+    }
+
+    // callback
+    public void onRenewableEnergyProviderRequest(String message) {
+        System.out.println("Received renewable energy provider request: " + message);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
